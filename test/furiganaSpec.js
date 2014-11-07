99b1aa470a4875713ce0a42b2c4c9febd86aafe8
vincent <vincent314@hotmail.fr>
2014-11-07 20:51:50 +0100

    Fix unit test

--- test/furiganaSpec.js
+++ test/furiganaSpec.js
@@ -1,19 +1,16 @@
 var furigana = require('../lib/furigana');
 var Showdown = require('showdown');
 var fs = require('fs');
-var expect = require('chai').expect();
 
 exports.showdown = {
     render: function (test) {
-        //var showdown = new Showdown.converter({extensions: [furigana]});
-        var showdown = new Showdown.converter();
+        var showdown = new Showdown.converter({extensions:[furigana]});
 
         var src = fs.readFileSync('./test/fixtures/furigana.md','utf8');
         var expected = fs.readFileSync('./test/expected/furigana.html','utf8');
-        console.log(src);
         var html = showdown.makeHtml(src);
-        console.log(html);
-        expect(html).toEqual(expected);
+
+        test.equals(html,expected);
         test.done();
     }
 };
